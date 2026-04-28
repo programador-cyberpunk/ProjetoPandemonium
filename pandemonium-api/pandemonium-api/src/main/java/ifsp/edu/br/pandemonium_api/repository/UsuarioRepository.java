1abe6c6c3a0406a2235f5b89d9ab58c9b89701d9
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-04-28 17:41:46 -0300

    Add files via upload

--- pandemonium-api/pandemonium-api/src/main/java/ifsp/edu/br/pandemonium_api/repository/UsuarioRepository.java
+++ pandemonium-api/pandemonium-api/src/main/java/ifsp/edu/br/pandemonium_api/repository/UsuarioRepository.java
@@ -3,7 +3,6 @@ import ifsp.edu.br.pandemonium_api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-    Optional<Usuario> findByUsername(String username);
-    boolean existsByUsername(String username, String senha);
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByUsernameAndSenha(String username, String senha);
 }
