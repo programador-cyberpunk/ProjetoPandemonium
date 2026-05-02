887958e7ae2640f62ed398636895fc1fd0ece9ff
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-05-01 22:38:06 -0300

    Corrigindo partes do DAO
    Ainda tem alguns erros, mas ta melhor logicamente falando

--- ProjetoPandemonium-master/src/Usuario/UsuariosDAO.java
+++ ProjetoPandemonium-master/src/Usuario/UsuariosDAO.java
@@ -6,33 +6,44 @@ import java.net.http.HttpClient;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 public class UsuariosDAO {
 
     private HttpClient httpClient = HttpClient.newHttpClient();
 
     public void apiLogin(String username, String senha) {
-
-    }
-
-
-    public void addUsuario(String usuario, String senha) {
-        httpClient.post("http://localhost:8080/api/usuarios", username, senha);
-    }
-
-    public void atualizarUsuario(Usuario usuarioAtualizado) {
-
-    }
-
-
-   /* public boolean deletarUsuario(int id) {
-        boolean removido = usuarios.removeIf(usuario -> usuario.getId() == id);
-        if (removido) {
-            System.out.println("Usuário de ID " + id + " foi chutado.");
-        } else {
-            System.out.println("Usuário de ID " + id + " não encontrado.");
+        //to chamando a api por essa requisição
+        String json = "{\"username\":\"" + username + "\", \"senha\":\"" + senha + "\"}";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/api/Usuario/login"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+        HttpRequest response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            if(response.statusCode() == 200){
+                System.out.println("Login efetuado com sucesso!");
+            }else {
+                System.out.println("Deu ruim no login: " + response.statusCode());
+            }catch(Exception.e){
+                e.printStackTrace();
         }
-        return removido;
+    }
+    public void addUsuario(String usuario, String senha){
+        try{
+            String json = "{\"username\":\"" + usuario + "\", \"senha\":\"" + senha + "\"}";
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/api/Usuarios/add"))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(json))
+                    .build();
+        httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    }
 
-    */
-}
+
+
