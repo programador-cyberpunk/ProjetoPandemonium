a653e8b3b563b4466f6e21c7fc2f65dff03b7adc
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-07-21 13:27:57 -0300

    Reescrevendo essa pagina

--- ProjetoPandemonium-master/src/TelaPrincipal.java
+++ ProjetoPandemonium-master/src/TelaPrincipal.java
@@ -6,14 +6,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.nio.file.Files;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import Magicka.AudioDAO;
+
 import Usuario.UsuariosDAO;
-import javax.swing.JFrame;
-import TelaLogin.TelaLogin;
-//import javazoom.jl.player.Player;//fodase isso aqui
+import javazoom.jl.player.Player;
+
     public class TelaPrincipal extends JFrame {
             public TelaPrincipal() {
                 super("Menu Principal - Pandemonium");
@@ -46,35 +42,35 @@ import TelaLogin.TelaLogin;
                 });
 
                 // implementando o botao pra fazer a magica acontecer no bom sentido
-                JButton btnUpload = new JButton("Selecionar audio");
-                btnUpload.addActionListener(new ActionListener(){
+                JButton btnUpload = new JButton("Selecionar Áudio");
+                btnUpload.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e){
                         JFileChooser fileChooser = new JFileChooser();
-                            FileNameExtensionFilter filtro = new FileNameExtensionFilter("Arquivo alguma coisa");
-                            fileChooser.setFileFilter(filtro);
-                            fileChooser.setAcceptAllFileFilterUsed(false);
-                                int retorno = fileChooser.showOpenDialog(null);
-                                    if(retorno == JFileChooser.APPROVE_OPTION){
-                                        File arquivo = fileChooser.getSelectedFile();
-                                    }
+                        //agora é onde o filtro começa a agir
+                        FileNameExtensionFilter filtro = new  FileNameExtensionFilter("Arquivos de AUDIO (*.mp3. *.wav, *.ogg,) mp3, wav, ogg.\n NÂO ME INVENTA MERDA !!!!!!");
+                        fileChooser.setFileFilter(filtro);
+                        fileChooser.setAcceptAllFileFilterUsed(false);
 
-                                    // porra do AudioDAO certo
-                                AudioDAO audioDAO = new AudioDAO();
-                                    if(audioDAO.processarUpload(arquivo, 1)){
-                                        JOptionPane.showMessageDialog(null, "Arquivo enviado e catalogado, gg");
-                                    }else {
-                                        JOptionPane.showMessageDialog(null, "Deu ruim, esse arquivo ta lombrado")
-                                    }
+                        int retorno = fileChooser.showOpenDialog(null);
+                        if (retorno == JFileChooser.APPROVE_VALUE){// sei la pq ta dando esse erro, eu fiz tudo certinho
+                            File arquivo = fileChooser.getSelectedFile();
+                        if(UsuariosDAO.arquivoAudio(audioSelecionado)){
+                            JOptionPane.showMessageDialog(null, "Arquivo selecionado com sucesso" + arquivo);
+                        }else {
+                            JOptionPane.showMessageDialog(null, "Deu ruim, esse arquivo eh invalido");
+                        }
+                        }
                     }
-                }
-                }
-            });
-
-            JButton itemGerenciadorChamados = new JButton("Gerenciador de chamados");
-                itemGerenciadorChamados.addActionListener(new ActionListener(){
+                });
+                AbstractButton itemGerenciadorChamados = null;// foi do jeito que deu porra
+                itemGerenciadorChamados.addActionListener(new ActionListener() {
                     @Override
-                     public void actionPerformed(ActionEvent e){
+                    public void actionPerformed(ActionEvent e) {
+
                         TelaGerenciadorChamados telaChamados = new TelaGerenciadorChamados();
+                        telaChamados.setVisible(true);
+                    }
+                });
+            }
         }
-    });
