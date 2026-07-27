32be1c8bd9d83e33a9338007f4fa649242f79ec4
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-07-27 00:21:15 -0300

    Add files via upload

--- src/Magicka/Armazenamento.java
+++ src/Magicka/Armazenamento.java
@@ -1,28 +1,34 @@
 package Magicka;
 import Usuario.Usuario;
+
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.InputStream;
-import javazoom.jl.player.Player;
+import javax.sound.sampled.*;
 import javax.swing.JFrame;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 // vamo importa tudo e torce pra dar certo
 
-public class Armazenamento {
-    //se pa, aqui vai ficar os arquivos, eu acho pelo menos
-    private final Path raiz = Paths.get("upload-dir");
-        public void salvarArmazenamento( String nomeArquivo, InputStream audioInput){
-            try{
-                if(!Files.exists(raiz)) Files.createDirectory(raiz);
-                Files.copy(audioInput, this.raiz.resolve(nomeAudio));
-                System.out.println("Taake salvo com sucesso em: " + nomeAudio); //nao estraga minha piada
-            } catch (Exception e){
-                throw new RuntimeException("Erro ao salvar arquivo: " + e.getMessage());
+public class Armazenamento{
+    private final Path raiz = Path.get("upload-dir");
+
+    public void salvarArmazenamento(String nomeArquivo, InputStream audioInput){
+        try{
+            if(!Files.exists(raiz)){
+                Files.createDirectories(raiz);
             }
+            Path destino = this.raiz.resolve(nomeArquivo);
+            Files.copy(audioInput, destino, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Armazenamento salvo com sucesso!GG!!!!!" + destino.toAbsolutePath());
+        }catch (IOException e){
+            throw new RuntimeException(e);
         }
+    }
 }
