0516d68ac5ef56e561b853d00aae1d1034ef1310
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-07-29 11:44:46 -0300

    Main reformulado

--- ProjetoPandemonium-master/src/Main.java
+++ ProjetoPandemonium-master/src/Main.java
@@ -8,21 +8,23 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.InputStream;
-import javafx.embed.JFXPanel;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
+import javafx.sound.sampled.*;
+import java.io.File;
 
 public class Main extends javax.swing.JFrame{
-    public static void main(String[] args) {
 
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
+    public static void main(String[] args){
+        try{
+            com.sun.javafx.application.PlataformImpl.startup(()-> {});
+        }catch (Exception ignored){
+            // ta no nome o que faz ne
+        }
+        SwingUtilities.invokerLater(new Runnable(){
+            Override
+            public void run(){
                 TelaLogin telaLogin = new TelaLogin();
-                telaLogin.setVisible(true); // AQUI COMEÇA O PANDEMONIUM
+                telaLogin.setVisible(true); // E COMEÇA O PANDEMONIUM
             }
         });
-
     }
-
 }
