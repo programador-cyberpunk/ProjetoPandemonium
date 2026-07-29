abc6d8cb8f4d13260a6dcac5f8cd011136da094e
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-07-28 23:21:03 -0300

    HÁ, agora ta operante

--- Chamado/Chamado.java
+++ Chamado/Chamado.java
@@ -4,54 +4,46 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Chamado extends JFrame {
+public class Chamado{
     private int id;
     private String titulo;
     private String descricao;
     private String status;
 
-    public Chamado(int id, String titulo, String descricao, String status) {
-        this.id = id;
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.status = status;
+        public Chamado(){}
+    public Chamado(int id, String titulo, String descricao, String status){
+            this.id = id;
+            this.titulo = titulo;
+            this.descricao = descricao;
+            this.status = status;
+    }
+    //getado e settado
+    public int getId(){
+            return id;
+    }
+    public void setId(int id){
+            this.id = id;
     }
 
-    public int getId() {
-        return id;
+    public String getTitulo(){
+            return titulo;
+    }
+    public void setTitulo(String titulo){
+            this.titulo = titulo;
     }
 
-    public void setId(int id) {
-        this.id = id;
+
+    public String getDescricao(){
+            return descricao;
+    }
+    public void setDescricao(String descricao){
+            this.descricao = descricao;
     }
 
-    public String getTitulo() {
-        return titulo;
+    public String getStatus(){
+            return status;
     }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public void setStatus(String status){
+            this.status = status;
     }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return getTitulo();
-    }
-
 }
