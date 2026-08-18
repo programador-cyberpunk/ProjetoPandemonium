edcc4694a7a6cbf7626c6008db2e6591a572bbfe
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-08-17 21:03:59 -0300

    Falta bastante coisa ainda

--- pandemonium-api/pandemonium-api/src/main/java/ifsp/edu/br/pandemonium_api/controller/AudioController.java
+++ pandemonium-api/pandemonium-api/src/main/java/ifsp/edu/br/pandemonium_api/controller/AudioController.java
@@ -1,18 +1,34 @@
 package com.pandemonium.controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.http.HttpRange;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaTypeFactory;
-import org.springframework.core.io.support,ResourceRegion;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.core.io.support.ResourceRegion;
+import java.util.List;
+import java.io.IOException;
+import ifsp.edu.br.pandemonium_api.model.Audio;
+import ifsp.edu.br.pandemonium_api.service.AudioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 @CrossOrigin(origins = "*")// pra liberar as rota dos bagulho
 @RestController
-@RequestMapping("/audios")
+@RequestMapping("/audio")
     public class AudioController{
-
+    @Autowired
+    private AudioService audioService;
+        @PostMapping("/upload")
+        public ResponseEntity<Audio> uploadAudio(@RequestParam("file") MultipartFile file,
+              @RequestParam(value = "idUsuario", required = false, defaultValue = "1"){
+            try{
+                Audio audioSalvo = audioService.salvaAudio(file, idUsuario);
+                    return ResponseEntity.ok(audioSalvo);
+            }catch(IOException e){
+                return ResponseEntity.internalServerError().build();
+            }
+        }
     @GetMapping
     public String listarAudios(){
         return "Lista dos audios";    }
