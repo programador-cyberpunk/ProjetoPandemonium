97dae79522ee7659fe87db579eca6f0abfdab51a
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-08-19 12:23:47 -0300

    refiz esa porra, mas ainda sem os modelos de teste

--- pandemonium-api/pandemonium-api/src/main/java/ifsp/edu/br/pandemonium_api/controller/AudioController.java
+++ pandemonium-api/pandemonium-api/src/main/java/ifsp/edu/br/pandemonium_api/controller/AudioController.java
@@ -1,53 +1,91 @@
-package com.pandemonium.controller;
-import org.springframework.core.io.Resource;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.core.io.support.ResourceRegion;
-import java.util.List;
-import java.io.IOException;
-import ifsp.edu.br.pandemonium_api.model.Audio;
-import ifsp.edu.br.pandemonium_api.service.AudioService;
-import org.springframework.beans.factory.annotation.Autowired;
+package ifsp.edu.br.pandemonium-api.controller;
+import ifsp.edu.br.pandemonium-api.model.Audio;
+import org.springframework.beans.factoryannotation.Autowired;
+import ifsp.edu.br.pandemonium-api.service.AudioService;
+import org.springfreamework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bin.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.List;
 
-@CrossOrigin(origins = "*")// pra liberar as rota dos bagulho
+@CrossOrifin(origins = "*") //pro front
 @RestController
-@RequestMapping("/audio")
+@RequestMapping("/api/audios")
     public class AudioController{
-    @Autowired
-    private AudioService audioService;
-        @PostMapping("/upload")
-        public ResponseEntity<Audio> uploadAudio(@RequestParam("file") MultipartFile file,
-              @RequestParam(value = "idUsuario", required = false, defaultValue = "1"){
-            try{
-                Audio audioSalvo = audioService.salvaAudio(file, idUsuario);
-                    return ResponseEntity.ok(audioSalvo);
-            }catch(IOException e){
-                return ResponseEntity.internalServerError().build();
-            }
-        }
-    @GetMapping
-    public String listarAudios(){
-        return "Lista dos audios";    }
-}
 
-// pra tocar o audio, ou garantir seu funciionamento
-@GetMapping
-public ResponseEntity<List<Audio>> listar() {
-    return ResponseEntity.ok(audioService.listarTodos());
-}
-    @GetMapping("/ouvir/{nomeArquivoSalvo}")
-    public ResponseEntity<Resource> ouvirAudio(@PathVariable String nomeArquivoSalvo){
+@Autowired
+private AudioService audioservice;
+    //rota de upload
+    @PostMapping("/upload")
+    public ResponseEntity<Audio> uploadAudio(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam(value = "idUsuario", required = false ,defaultValue =  "1" ) Integer idUsuario){
         try{
-            Resource recurso = audioService.carregarArquivo(nomeArquivoSalvo);
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION,"inline; filename=\"" + recurso.getFilename() + "\"")
-                    .contentType(MediaType.parseMediaType("audio/mpeg"))
-                    .body(recurso);
-        }  catch(IOException e){
-           return ResponseEntity.notFound().build();
+            Audio audioSalvo = audioService.salvarAudio(file, idUsuario);
+            return ResponseEntity.ok(audioSalvo);
+        } catch(IOException e){
+            return ResponseEntity.internalServerError().build();
         }
     }
+
+    // rota das listas
+   @GetMapping
+   public ResponseEntity<List<Audio>> listar(){
+        return ResponseEntity.ok(audioService.listarTodos());
+   }
+
+// PRA TOCAR O ADIO NO APP
+@GetMapping("/ouvir/{nomeArquivoSalvo}")
+public ResponseEntity<Resource> ouvirAudio(@PathVariable String nomeArquivoSalvo){
+        try{
+          Resource recurso = audioService.carregarArquivo(nomeArquivoSalvo);
+          return ResponseEntity.ok()
+                  .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + recurso.getFilename() + "\"")
+                  .contentType(MediaType.parseMediaType("audio/mpeg"))
+                  .body(recurso);
+        } catch(IOException e){
+            return ResponseEntity.notFound().build();
+        }
+}
+
+@GetMapping("/stream/{nomeArquivoSalvo}")
+    public ResponseEntity<ResourceRegion> streamAudio(
+            @PathVariable String nomeArquivoSalvo,
+            @RequestHeaders headers) {
+        try{
+            Resource recurso = audioService.carregarArquivo(nomeArquivoSalvo);
+            long contentLength = recurso.contentLength();
+
+            HttpRange range = headers.getRange().isEmpty() ? null : headers.getRange().get(0);
+            ResourceRegion region;
+
+                if(range != null){
+                    long start = range.getRangeStart(contentLength);
+                    long end = range.getRangeEnd(contentLength);
+                    long rangeLength = Math.min(1024 * 1024, end - start + 1);
+                    region = new ResourceRegion(recurso, start, rangeLength);
+                }else{
+                    long rangeLentgh = Math,min(1024 * 1024L, contentLength);
+                    region = new ResourceRegion(recurso, 0 ,rangeLentgh);
+                }
+             return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                     .contentType(MediaTypeFactory.getMediaType(recurso))
+                     .body(region);
+        }catch(IOException e){
+            return ResponseEntity.notFound().build();
+        }
+}
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void>deletarAudioo(@PathVariable Long id){
+        try{
+            audioService.deletarAudio(id);
+            return ReponseEntity.noContent().build();
+        }catch(IOException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+)
+}
