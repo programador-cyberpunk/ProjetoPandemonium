3c6c9cd3736011a0dae2bad43be8d62aa9402bfc
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-09-22 23:04:15 -0300

    PANDEMONIUM VIVE PORRA!!!!!

--- pandemonium-api/pandemonium-api/src/main/java/ifsp/edu/br/pandemonium_api/model/Armazenamento.java
+++ pandemonium-api/pandemonium-api/src/main/java/ifsp/edu/br/pandemonium_api/model/Armazenamento.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-    public class Guardar{
+    public class Armazenamento{
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
